f2b0f7442f1cd3b9684dd701ec01a0bce6a13c28
irina <irinakyleashova2519@gmail.com>
2022-10-05 01:46:18 +0300

    hello

---
+++ zadachi/hello/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("введите свое имя");
+string username=Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("ура, это же МАША!");
+}
+else
+{
+    Console.Write("привет,");
+    Console.WriteLine(username);
+}
+
